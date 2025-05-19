6921d04ef75a152ffc38239dffa7e1f57d9d3d82
Gustavo Teider <gustavoteidet@gmail.com>
2025-05-19 18:36:30 -0300

    urls att

--- projeto/urls.py
+++ projeto/urls.py
@@ -15,8 +15,20 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import path
+
+
+def my_view(request):
+    return HttpResponse('UMA STRING')
+
+
+def my_view2(request):
+    return HttpResponse('MAIN PAGE')
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', my_view2),
+    path('sobre/', my_view),
 ]
