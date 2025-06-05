3f5bc581f2ad97ddcd2a9e1ea5e862a999174ca5
Gustavo Teider <gustavoteidet@gmail.com>
2025-06-05 17:28:41 -0300

    att57

--- projeto/settings.py
+++ projeto/settings.py
@@ -116,6 +116,11 @@ SATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
 
+STATIC_ROOT = BASE_DIR / 'static'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
