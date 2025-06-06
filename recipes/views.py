55d275beb4eea5952f5a4a672ae8450b325d608f
Gustavo Teider <gustavoteidet@gmail.com>
2025-06-06 17:09:11 -0300

    att 61

--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 from recipes.models import Recipe
@@ -7,7 +8,7 @@ from utils.recipes.factory import make_recipe
 
 
 def home(request):
-    recipes = Recipe.objects.all().order_by('-id')
+    recipes = Recipe.objects.filter(is_published=True).order_by('-id')
     return render(request, 'recipes/pages/home.html', context={
         'recipes': recipes,
     })
@@ -15,9 +16,16 @@ def home(request):
 
 def category(request, category_id):
     recipes = Recipe.objects.filter(
-        category__id=category_id).order_by('-id')
-    return render(request, 'recipes/pages/home.html', context={
+        category__id=category_id,
+        is_published=True
+    ).order_by('-id')
+
+    if not recipes:
+        return HttpResponse('Not Found', status=404)
+
+    return render(request, 'recipes/pages/category.html', context={
         'recipes': recipes,
+        'title': f'{recipes.first().category.name} - Category |',
     })
 
 
